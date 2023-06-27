c86867b81184e57004ede8686de694d3edf3ac81
Ethan Padden <ethanpadden@gmail.com>
2023-06-27 09:53:52 +0100

    Add login screen

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.set('view engine', 'ejs');
 
 const server = app.listen(3000); 
 
-app.get('/', (req,res) => {
-    res.send('<p>Hello World!</p>');
+app.get('/login', (req,res) => {
+    res.render('login');
 });
